431f95492f0dfcdf868ed6db375ebaf5fc778391
Aqui <aquiren@proton.mail>
2026-03-13 04:36:52 +0500

    Create arrays

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
+package main
+
 import (
 	"fmt"
 )
 
-func main()
-{
-	fmt.Print("Empty file\n")
+func main() {
+	var input_array [256]bool
+	var key [256]bool
+
+	var output_array [256]bool
+	for i:=0; i<len(input_array); i++ {
+		output_array[i] = input_array[i] == key[i]
+	}
+	fmt.Println(output_array[0]);
 }
